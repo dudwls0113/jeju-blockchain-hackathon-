15605bfc0614b8fd72d897c6e1e5afa392ed87b6
문영진 <dudwls0113@gmail.com>
2019-11-07 19:39:34 +0900

    OPEN CV연결

--- JejuBlockChainHackathon/app/src/main/java/com/jejublockchainhackathon/src/main/interfaces/MainActivityView.java
+++
@@ -1,8 +0,0 @@
-package com.jejublockchainhackathon.src.main.interfaces;
-
-public interface MainActivityView {
-
-    void validateSuccess(String text);
-
-    void validateFailure(String message);
-}
---
+++ app/src/main/java/com/hackthon/jeju_block_chain/src/main/interfaces/MainActivityView.java
@@ -0,0 +1,8 @@
+package com.hackthon.jeju_block_chain.src.main.interfaces;
+
+public interface MainActivityView {
+
+    void validateSuccess(String text);
+
+    void validateFailure(String message);
+}
