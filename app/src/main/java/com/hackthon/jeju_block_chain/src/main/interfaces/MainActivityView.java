af6880aeb401f2d3b48dd325e88d5ede29674dfe
문영진 <dudwls0113@gmail.com>
2019-11-07 22:51:42 +0900

    ㅇ

--- app/src/main/java/com/hackthon/jeju_block_chain/src/main/interfaces/MainActivityView.java
+++
@@ -1,8 +0,0 @@
-package com.hackthon.jeju_block_chain.src.main.interfaces;
-
-public interface MainActivityView {
-
-    void validateSuccess(String text);
-
-    void validateFailure(String message);
-}
